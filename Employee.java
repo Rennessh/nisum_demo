9333341e78099e6eb05fb4bd80ebe588b3aed1a7
Oruganti Rennessh Reddy <oreddy@nisum.com>
2022-09-23 17:33:59 +0530

    first file is added to repoistry

---
+++ Employee.java
@@ -0,0 +1,14 @@
+public class Employee{
+private int empid;
+private String ename;
+private float salary;
+
+Employee(){}
+Employee(int empid , String ename, float salary)
+{
+this.empid=empid;
+this.ename=ename;
+this.salary=salary;
+}
+
+}
